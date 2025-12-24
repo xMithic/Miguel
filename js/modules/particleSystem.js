615baf243e65c2d1be6ec87c64f410a2110797db
MiguelMarca <144856418+xMithic@users.noreply.github.com>
2025-12-24 00:21:59 -0400

    Update particleSystem.js

--- js/modules/particleSystem.js
+++ js/modules/particleSystem.js
@@ -145,7 +145,7 @@ export class ParticleSystem {
         this.audioAnalyzer = audioAnalyzer;
         this.particles = [];
         this.MAX_PARTICLES = 200;
-        this.CONNECTION_DISTANCE = 100;
+        this.CONNECTION_DISTANCE = 170;
         this.MAX_CONNECTIONS_PER_PARTICLE = 3;
 
         // -- INTERACTIVIDAD --
